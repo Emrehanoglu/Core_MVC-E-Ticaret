c74c6a0cdcfda739e9e2cbb2cf4f8b42b32817fa
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-12-25 00:18:50 +0300

    ShopApp #10

---
+++ ShopApp.WebUILayer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUILayer.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IProductService _productService;
+
+		public HomeController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_productService.GetAll());
+		}
+	}
+}
---
+++ ShopApp.WebUILayer/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUILayer.Controllers
+{
+	public class ProductController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
