84a694e0a05658df706947fddec8e81176c9a193
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-12-24 11:18:04 +0300

    ShopApp #4

---
+++ ShopApp.Entities/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Entities
+{
+	public class ProductCategory
+	{
+		//coka cok ilişki
+		public int CategoryId { get; set; }
+		public Category Category { get; set; }
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
+		public List<ProductCategory> ProductCategories { get; set; }
+	}
+}
