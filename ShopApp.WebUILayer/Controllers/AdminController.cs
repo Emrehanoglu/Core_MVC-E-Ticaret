d62ec4f61c6448913821a03feca2c5eedb2aacab
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-12-28 23:35:54 +0300

    ShopApp #23

--- ShopApp.WebUILayer/Controllers/AdminController.cs
+++ ShopApp.WebUILayer/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using ShopApp.WebUILayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,33 @@ namespace ShopApp.WebUILayer.Controllers
 {
 	public class AdminController : Controller
 	{
+		private IProductService _productService;
+		public AdminController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+		[HttpGet]
+		public IActionResult CreateProduct()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CreateProduct(ProductModel model)
+		{
+			var entity = new Product()
+			{
+				Name = model.Name,
+				Price = model.Price,
+				Description = model.Description,
+				ImageUrl = model.ImageUrl
+			};
+			_productService.Create(entity);
+			return Redirect("Index");
 		}
 	}
 }
