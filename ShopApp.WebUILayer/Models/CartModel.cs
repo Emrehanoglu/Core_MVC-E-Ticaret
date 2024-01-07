a2ff9c50e3d1c1221b2ee48c3d2c3b3e7ec2aaae
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-07 12:44:53 +0300

    ShopApp #52

--- ShopApp.WebUILayer/Models/CartModel.cs
+++ ShopApp.WebUILayer/Models/CartModel.cs
@@ -9,6 +9,10 @@ namespace ShopApp.WebUILayer.Models
 	{
 		public int CartId { get; set; }
 		public List<CartItemModel> CartItems { get; set; }
+		public decimal TotalPrice()
+		{
+			return CartItems.Sum(x => x.Price * x.Quantity);
+		}
 	}
 	public class CartItemModel
 	{
